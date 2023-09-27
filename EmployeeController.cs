6317e2c43ac9fde14566728ee81965feed5ca7c0
shereen <shereen@DESKTOP-AHOU8JD>
2023-09-27 23:53:53 +0300

    Add project files.

---
+++ EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Lap2.Models;
+
+namespace MVC_Lap2
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext context;
+        public EmployeeController()
+        {
+                context = new EmployeeContext();
+        }
+        public IActionResult Index()
+        {
+            List<Employee> employees = context.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult Details(int id)
+        {
+            Employee employee=context.Employees.Where(e=>e.id==id).SingleOrDefault();
+            if(employee==null) { 
+          return Content("Error");
+            }
+            return View(employee);
+        }
+        public IActionResult NewForm()
+        {
+            return View();
+        }
+        public IActionResult addactionresult(Employee employee)
+        {
+            context.Employees.Add(employee);
+          
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Migrations/20230927072021_v1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Lap2.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Office",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    location = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Office", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    age = table.Column<int>(type: "int", nullable: true),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    salary = table.Column<int>(type: "int", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    office_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Office_office_id",
+                        column: x => x.office_id,
+                        principalTable: "Office",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees_Projects",
+                columns: table => new
+                {
+                    emp_id = table.Column<int>(type: "int", nullable: false),
+                    prj_id = table.Column<int>(type: "int", nullable: false),
+                    workinghours = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees_Projects", x => new { x.emp_id, x.prj_id });
+                    table.ForeignKey(
+                        name: "FK_Employees_Projects_Employees_emp_id",
+                        column: x => x.emp_id,
+                        principalTable: "Employees",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Employees_Projects_Projects_prj_id",
+                        column: x => x.prj_id,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_office_id",
+                table: "Employees",
+                column: "office_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Projects_prj_id",
+                table: "Employees_Projects",
+                column: "prj_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees_Projects");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Office");
+        }
+    }
+}
